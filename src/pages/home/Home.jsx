f39024e0b88e61034b5a966e2799ae354fe47ab8
acerbastimur <umutacerbastimur@gmail.com>
2020-05-03 22:48:54 +0300

    Home page created

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react"; 
+class Home extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+       <h1>hello creatella</h1>
+      </>
+    );
+  }
+}
+
+export default Home;
